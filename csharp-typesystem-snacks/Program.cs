48676965e9dd4de231fe0688076f2484e3f9b3c9
mikaeltreppo <mikaeltreppo@outlook.com>
2023-09-06 14:40:34 +0200

    Terzo snack

--- csharp-typesystem-snacks/Program.cs
+++ csharp-typesystem-snacks/Program.cs
@@ -35,6 +35,22 @@ else
     Console.WriteLine("Le parole sono uguali");
 }
 //Snack 3: Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
+Console.WriteLine("Inserisci 10 numeri, ti restituirò la somma totale");
+
+int sum = 0;
+
+for (int i = 0; i < 10; i++)
+{
+
+  
+    sum = sum + int.Parse(Console.ReadLine());
+
+
+}
+
+Console.WriteLine(sum);
+
+
 //Snack 4: Calcola la somma e la media dei numeri da 2 a 10.
 //Snack 5: Il software chiede all’utente di inserire un numero. Se il numero inserito è pari, stampa il numero, se è dispari, stampa il numero successivo.
 //Snack 6: In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby. Chiedi all’utente il suo nome e comunicagli se può partecipare o meno alla festa.
